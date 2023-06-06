f21dd392605556816901648ae8d4c57ad5d1dd87
fendishatech <liyukibir@gmail.com>
2023-06-06 02:38:19 -0700

    header and about section

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 const ExpressApp = require("./src/app");
 // --- Dev ---
-const migrate_tables = require("./src/helpers/migrate_models");
-migrate_tables();
+// const migrate_tables = require("./src/helpers/migrate_models");
+// migrate_tables();
 // --- Dev ---
 const app = new ExpressApp();
 app.startServer(3333);
